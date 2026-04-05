c2e8f3921200db084d750c05114fc986a2a39d7a
Ruab1206 <rune.abrahamsson@gmail.com>
2026-04-05 12:56:14 +0000

    Force Vercel rebuild with clean cache

--- src/main.tsx
+++ src/main.tsx
@@ -6,6 +6,8 @@ import CataloguePage from './pages/CataloguePage'
 import './index.css'
 import { ThemeProvider } from './hooks/useTheme'
 
+// Force rebuild cache bust
+
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <ThemeProvider>
